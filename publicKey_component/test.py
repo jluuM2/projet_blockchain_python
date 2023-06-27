1bde6daba56cc8ca5418fafac900012f2a5c39a9
Dahuiss <76972002+Dahuiss@users.noreply.github.com>
2023-06-27 18:39:26 +0200

    Update test.py

--- publicKey_component/test.py
+++ publicKey_component/test.py
@@ -1,4 +1,6 @@
 import ecdsa
+import base64
+import json
 from ecdsa.util import sigencode_string
 from PublicKeyRecoveryComponent import PublicKeyRecoveryComponent
 
@@ -11,11 +13,15 @@ def test_component():
     message = b"test message"
     signature = private_key.sign(message, sigencode=sigencode_string)
 
+    # Encode the signature in base64 and wrap it in a JSON string
+    signature_base64 = base64.b64encode(signature).decode()
+    signature_json = json.dumps({"signature": signature_base64})
+
     # Create a PublicKeyRecoveryComponent instance
     recovery_component = PublicKeyRecoveryComponent(ecdsa.SECP256k1)
 
     # Test the take_signature method
-    recovery_component.take_signature(signature)
+    recovery_component.take_signature(signature_json)
     print("take_signature passed")
 
     # Test the validate_signature_format method
