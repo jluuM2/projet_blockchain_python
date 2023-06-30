e54bbe09904fb092b14e143f0d73e5bc94c683f4
yufei-liu-source <liuyufei9931@gmail.com>
2023-06-30 19:47:38 +0200

    upgrade

--- publicKey_component/public_key.cpp
+++ publicKey_component/public_key.cpp
@@ -1,74 +1,55 @@
 #include <stdexcept>
 #include <string>
-#include <openssl/ec.h>
-#include <openssl/ecdsa.h>
-#include <openssl/obj_mac.h>
 #include <pybind11/pybind11.h>
-
-/**
- * Recovers a public key from a signed message using ECDSA.
- * @param signature: The signature of the signed message.
- * @param message: The original message.
- * @returns: The recovered public key.
- * @throws std::runtime_error: If the recovery of the public key fails.
- */
-#include "uECC.h"
 #include <stdio.h>
 #include <stdexcept>
 #include <string>
+#include <secp256k1_recovery.h>
 
-std::string recover_public_key(const std::string& signature_hex, const std::string& message_hex) {
-    // Verify that the signature and message are in hexadecimal format
-    if (signature_hex.empty() || signature_hex.substr(0, 2) != "0x" ||
-        message_hex.empty() || message_hex.substr(0, 2) != "0x") {
-        throw std::invalid_argument("The signature and message must be in hexadecimal format (e.g., '0x1a2b3c...', '0x4d657373616765')");
+std::string recover_public_key(std::string signature) {
+    // Check if the signature starts with '0x' and remove it
+    if (signature.substr(0, 2) == "0x") {
+        signature = signature.substr(2);
     }
 
-    // Convert the signature and message from hexadecimal to binary
-    size_t signature_size = (signature_hex.size() - 2) / 2;
-    size_t message_size = (message_hex.size() - 2) / 2;
-    uint8_t signature[uECC_BYTES * 2];
-    uint8_t message[uECC_BYTES * 2];
-    for (size_t i = 0; i < signature_size; ++i) {
-        std::string byte_string = signature_hex.substr(i * 2 + 2, 2);
-        signature[i] = static_cast<uint8_t>(std::stoi(byte_string, nullptr, 16));
-    }
-    for (size_t i = 0; i < message_size; ++i) {
-        std::string byte_string = message_hex.substr(i * 2 + 2, 2);
-        message[i] = static_cast<uint8_t>(std::stoi(byte_string, nullptr, 16));
+    // Convert hex string to byte array
+    std::vector<unsigned char> signatureBytes(signature.length() / 2);
+    for (unsigned int i = 0; i < signature.length(); i += 2) {
+        std::string byteString = signature.substr(i, 2);
+        unsigned char byte = (unsigned char) strtol(byteString.c_str(), NULL, 16);
+        signatureBytes[i / 2] = byte;
     }
 
-    // Iterate through possible public keys and verify the signature
-    std::string public_key_hex;
-    for (size_t i = 0; i < uECC_CURVE_COUNT; ++i) {
-        const struct uECC_Curve_t* curve = uECC_get_curve_by_index(i);
-        uint8_t public_key[uECC_BYTES * 2];
-        
-        // Compute the public key for the current curve
-        if (!uECC_compute_public_key(signature, message, message_size, public_key, curve)) {
-            continue; // Invalid public key for this curve, try the next one
-        }
-
-        // Verify if the computed public key produces the given signature
-        if (uECC_verify(public_key, message, message_size, signature, curve)) {
-            // Convert the public key from binary to hexadecimal
-            public_key_hex.clear();
-            for (size_t j = 0; j < uECC_BYTES * 2; ++j) {
-                char byte_string[3];
-                sprintf(byte_string, "%02x", public_key[j]);
-                public_key_hex += byte_string;
-            }
-            break; // Found a valid public key, exit the loop
-        }
+    if (signatureBytes.size() != 65) {
+        throw std::invalid_argument("Invalid signature length");
     }
 
-    if (public_key_hex.empty()) {
+    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY | SECP256K1_CONTEXT_SIGN);
+    secp256k1_ecdsa_recoverable_signature sig;
+    if (!secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &sig, signatureBytes.data(), signatureBytes[64])) {
+        throw std::invalid_argument("Invalid signature format");
+    }
+
+    secp256k1_pubkey pubkey;
+    if (!secp256k1_ecdsa_recover(ctx, &pubkey, &sig, signatureBytes.data())) {
         throw std::runtime_error("Failed to recover public key");
     }
 
-    return public_key_hex;
-}
+    char serialized_pubkey[65];
+    size_t len = 65;
+    secp256k1_ec_pubkey_serialize(ctx, (unsigned char*)serialized_pubkey, &len, &pubkey, SECP256K1_EC_UNCOMPRESSED);
 
+    secp256k1_context_destroy(ctx);
+    
+    // Convert the public key to a hex string
+    std::string pubkeyHex;
+    for (unsigned int i = 0; i < len; i++) {
+        char hex[3];
+        sprintf(hex, "%02x", (unsigned char)serialized_pubkey[i]);
+        pubkeyHex += hex;
+    }
+    return pubkeyHex;
+}
 
 /**
  * Pybind11 module definition.
